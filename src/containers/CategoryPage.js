4feecd59346ba5139976446ee63fa53c91a922e3
ozanzeng <oznzengin@gmail.com>
2020-07-09 09:21:46 +0300

    Kategori sayfası tamamlandı

--- src/containers/CategoryPage.js
+++ src/containers/CategoryPage.js
@@ -1,18 +1,80 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
+import PageTitle from '../components/PageTitle';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
-export class CategoryPage extends Component {
-  static propTypes = {
+const CategoryPage = () => {
+  const getConfigurableProps = () => ({
+    showArrows: false,
+    showThumbs: false,
+  });
 
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>CategoryPage</h1>
+  return (
+    <>
+      <div className="category-content">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <PageTitle title="Saklama ve Düzenleme" />
+              <Carousel {...getConfigurableProps()}>
+                <div>
+                  <div className="row">
+                    <div className="col-md-3">
+                      <div className="category">
+                        <Link to="/kategori-urunleri">
+                          <img src="/images/kategori.png" alt="" />
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="col-md-3">
+                      <div className="category">
+                        <Link to="/kategori-urunleri">
+                          <img src="/images/kategori.png" alt="" />
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="col-md-3">
+                      <div className="category">
+                        <Link to="/kategori-urunleri">
+                          <img src="/images/kategori.png" alt="" />
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="col-md-3">
+                      <div className="category">
+                        <Link to="/kategori-urunleri">
+                          <img src="/images/kategori.png" alt="" />
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className="row">
+                    <div className="col-md-3">
+                      <div className="category">
+                        <Link to="/kategori-urunleri">
+                          <img src="/images/kategori.png" alt="" />
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="col-md-3">
+                      <div className="category">
+                        <Link to="/kategori-urunleri">
+                          <img src="/images/kategori.png" alt="" />
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Carousel>
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
-}
+    </>
+  );
+};
 
-export default CategoryPage
+export default CategoryPage;
