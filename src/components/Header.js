32310e3f4c859d5e331c5c26b4e631105b51b784
ozanzeng <oznzengin@gmail.com>
2020-08-24 23:31:12 +0300

    Header alanin slider olmasi - Text guncellemesi Header.js icinden yapiliyor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import Carousel from 'react-elastic-carousel';
 import Menu from './Menu';
 
 const Header = ({ paths, homeHeader }) => {
@@ -9,11 +10,28 @@ const Header = ({ paths, homeHeader }) => {
     i18n.changeLanguage(lng);
   };
 
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 765, itemsToShow: 2 },
+    { width: 850, itemsToShow: 3, itemsToScroll: 3 },
+    { width: 1150, itemsToShow: 4, itemsToScroll: 4 },
+    { width: 1450, itemsToShow: 5 },
+    { width: 1750, itemsToShow: 6 },
+  ];
+
   const createLinks = (
-    <ul>
-      <li>Kategori 1</li>
-      <li>Kategori 2</li>
-    </ul>
+    <Carousel breakPoints={breakPoints}>
+      <div className="menuLink">Kategori 1</div>
+      <div className="menuLink">Kategori 2</div>
+      <div className="menuLink">Kategori 3</div>
+      <div className="menuLink">Kategori 4</div>
+      <div className="menuLink">Kategori 5</div>
+      <div className="menuLink">Kategori 6</div>
+      <div className="menuLink">Kategori 7</div>
+      <div className="menuLink">Kategori 8</div>
+      <div className="menuLink">Kategori 9</div>
+      <div className="menuLink">Kategori 10</div>
+    </Carousel>
   );
 
   const detectLanguage = 'TR';
